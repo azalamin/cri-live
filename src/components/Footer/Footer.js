3f56576f45dd78ffc923127320847f49dc118f38
Al-amin Sheikh <azalamins@gmail.com>
2022-06-10 19:08:14 +0600

    done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { AiFillChrome } from "react-icons/ai";
+import { BsFacebook, BsTelephoneFill } from "react-icons/bs";
 import './Footer.css';
+
+
 
 const Footer = () => {
     return (
-        <div>
-            
+      <footer>
+        <div className="container footer_container">
+          <div className="icon-container">
+            <BsTelephoneFill className="icon-phone icon" />
+            <p className='footer-item'>
+              <a href="tell:1800 200 1234">Toll free 1800 200 1234</a>
+            </p>
+          </div>
+          <div className="icon-container">
+            <BsFacebook className="icon" />
+            <p className='footer-item'>
+              <a href="https://www.facebook.com/cribumps">
+                https://www.facebook.com/cribumps
+              </a>
+            </p>
+          </div>
+          <div className="icon-container">
+            <AiFillChrome className="icon" />
+            <p className='footer-item'>
+              <a href="https://www.crigroups.com/">
+                https://www.crigroups.com/
+              </a>
+            </p>
+          </div>
         </div>
+      </footer>
     );
 };
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Footer from '../Footer/Footer';
 import Products from '../Products/Products';
 import './Home.css';
 
 const Home = () => {
     return (
-        <div className='container'>
-            <Banner />
-            <Products />
+      <>
+        <div className="container">
+          <Banner />
+          <Products />
         </div>
+        <Footer />
+      </>
     );
 };
 
